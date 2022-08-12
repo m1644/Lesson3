a8ac5469641332392c4a0c6ad4acf5825390e7f0
MaxX <maxx-f@mail.ru>
2022-08-12 14:31:46 +0300

    Add Task002

---
+++ Task002/Program.cs
@@ -0,0 +1,21 @@
+﻿// Напишите программу, которая по заданному номеру четверти,
+// показывает диапазон возможных координат точек в этой четваерти (X и Y).
+
+void FindRange(int number)
+{
+    if (number == 1)
+        System.Console.WriteLine("x > 0, y > 0");
+    else if (number == 2)
+        System.Console.WriteLine("x < 0, y > 0");
+    else if (number == 3)
+        System.Console.WriteLine("x < 0, y < 0");
+    else if (number == 4)
+        System.Console.WriteLine("x > 0, y < 0");
+    else
+        System.Console.WriteLine("Неверные данные");    
+}
+FindRange(1);
+FindRange(2);
+FindRange(3);
+FindRange(4);
+FindRange(0);
